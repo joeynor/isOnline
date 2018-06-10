62fa4186ae69693b166c821e8e822f23e8299333
Rizal Mohd Nor <joey@Rizals-MacBook-Pro.local>
2018-06-10 11:49:41 +0800

    Just to make it work for my mining purposes

--- checker.sh
+++ checker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Website status checker. by ET (etcs.me)
 
-WORKSPACE=/scripts/isOnline
+WORKSPACE=/Users/joey/Downloads/mining_scripts/isOnline
 # list of websites. each website in new line. leave an empty line in the end.
 LISTFILE=$WORKSPACE/websites.lst
 # Send mail in case of failure to. leave an empty line in the end.
 EMAILLISTFILE=$WORKSPACE/emails.lst
 # Temporary dir
 TEMPDIR=$WORKSPACE/cache
+TIMETOREBOOT=30
 
 # `Quiet` is true when in crontab; show output when it's run manually from shell.
 # Set THIS_IS_CRON=1 in the beginning of your crontab -e.
@@ -23,21 +24,27 @@ function test {
     # website working
     if [ "$QUIET" = false ] ; then
       echo -n "$response "; echo -e "\e[32m[ok]\e[0m"
+      # should do nothing
     fi
     # remove .temp file if exist 
     if [ -f $TEMPDIR/$filename ]; then rm -f $TEMPDIR/$filename; fi
   else
     # website down
-    if [ "$QUIET" = false ] ; then echo -n "$response "; echo -e "\e[31m[DOWN]\e[0m"; fi
-    if [ ! -f $TEMPDIR/$filename ]; then
-        while read e; do
-            # using mailx command
-            echo "$p WEBSITE DOWN" | mailx -s "$1 WEBSITE DOWN ( $response )" $e
-            # using mail command
-            #mail -s "$p WEBSITE DOWN" "$EMAIL"
-        done < $EMAILLISTFILE
-        echo > $TEMPDIR/$filename
+    if [ "$QUIET" = false ] ; then 
+	echo -n "$response "; echo -e "\e[31m[DOWN]\e[0m"; 
+	# should shutdown reboot the server 
+	/sbin/shutdown -r +$TIMETOREBOOT; 
     fi
+    # comment out cause if dont want to send emails
+#    if [ ! -f $TEMPDIR/$filename ]; then
+#        while read e; do
+#            # using mailx command
+#            echo "$p WEBSITE DOWN" | mailx -s "$1 WEBSITE DOWN ( $response )" $e
+#            # using mail command
+#            #mail -s "$p WEBSITE DOWN" "$EMAIL"
+#        done < $EMAILLISTFILE
+#        echo > $TEMPDIR/$filename
+#    fi
   fi
 }
 
